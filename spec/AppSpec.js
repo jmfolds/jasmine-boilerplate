b84fd455656426ddf35a8c9b44e50f4886930a30
Jeremy Folds <jmfolds@Jeremys-MacBook-Pro.local>
2014-04-03 19:08:55 -0600

    looking at spies

--- spec/AppSpec.js
+++ spec/AppSpec.js
@@ -3,10 +3,11 @@
 /*globals vw*/
 
 describe("App", function() {
-  var app;
+  var app, speak;
 
   beforeEach(function() {
     app = vw.App;
+    // speak = vw.App.tellMeSomething;
   });
 
   it("App should not be null", function() {
@@ -16,5 +17,41 @@ describe("App", function() {
   it("stubfields length should be 7", function() {
       expect(app.stubFields.length).toEqual(7);
   });
+  describe("tellMeSomething", function() {
+    it("should throw an exception without valid params", function() {
+      expect(function() {
+        app.tellMeSomething('Jeremy', null);
+      }).toThrow('Missing parameters!');
+    });
+  });
+});
 
+describe("A spy", function() {
+  var foo, bar = null;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      }
+    };
+
+    spyOn(foo, 'setBar');
+
+    foo.setBar(123);
+    foo.setBar(456, 'another param');
+  });
+
+  it("tracks that the spy was called", function() {
+    expect(foo.setBar).toHaveBeenCalled();
+  });
+
+  it("tracks all the arguments of its calls", function() {
+    expect(foo.setBar).toHaveBeenCalledWith(123);
+    expect(foo.setBar).toHaveBeenCalledWith(456, 'another param');
+  });
+
+  it("stops all execution on a function", function() {
+    expect(bar).toBeNull();
+  });
 });
--- src/app.js
+++ src/app.js
@@ -12,4 +12,12 @@ if (!this.vw || typeof this.vw !== 'object') {
 
     vw.App.stubFields = [1, 2, 3, 4, 5, 6, 7];
 
+    vw.App.tellMeSomething = function (name, msg) {
+        if (!msg || !name) {
+            throw 'Missing parameters!';
+        }
+        
+        return 'Hi, ' + name + '. ' + msg;
+    };
+
 })();
